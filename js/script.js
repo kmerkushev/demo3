f92d9564283c55cfc375fdd15c55d60324bd0085
kmerkushev <regesc@yandex.ru>
2024-12-31 22:27:02 +0530

    add pdfs to desktop

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const dawn = document.querySelector(`#dawn`);
+const helium = document.querySelector(`#helium`);
+const glow = document.querySelector(`#glow`);
+const geodnet = document.querySelector(`#geodnet`);
+const hivemapper = document.querySelector(`#hivemapper`);
+const grass = document.querySelector(`#grass`);
+const io = document.querySelector(`#io`);
+const sourceful = document.querySelector(`#sourceful`);
+const fuse = document.querySelector(`#fuse`);
+
+
+
+
+
+
+
+
